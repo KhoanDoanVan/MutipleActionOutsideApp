9bde3fc9dd529c6e0d95325a225edb9e1920b730
khoanisdev <doanvankhoan124@gmail.com>
2025-02-27 14:37:20 +0700

    Initial Commit

---
+++ AppIntent-Siri,QuickAction,Widget/AppIntent_Siri_QuickAction_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntent_Siri_QuickAction_WidgetApp.swift
+//  AppIntent-Siri,QuickAction,Widget
+//
+//  Created by Đoàn Văn Khoan on 27/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AppIntent_Siri_QuickAction_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIntent-Siri,QuickAction,Widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppIntent-Siri,QuickAction,Widget
+//
+//  Created by Đoàn Văn Khoan on 27/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
